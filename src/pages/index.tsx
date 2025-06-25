d9ef7372ffced02df6bf02b3d1e3fe80013bbfcd
mnq78 <nahuelq.78@gmail.com>
2025-06-25 13:50:52 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+// pages/index.tsx
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-2xl font-semibold text-gray-700">
+        Welcome to your new Next.js + Tailwind CSS project!
+      </h1>
+    </main>
+  );
+}
